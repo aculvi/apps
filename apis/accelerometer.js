d9418fbd52c6d23c1c161c42c000bf4a5ef59d82
aculvi <aculvi@gmail.com>
2014-04-15 10:46:34 +0200

    0.0.4

--- apis/accelerometer.js
+++ apis/accelerometer.js
@@ -1,69 +1,46 @@
-/* Copyright (c) 2012 Mobile Developer Solutions. All rights reserved.
- * This software is available under the MIT License:
- * The MIT License
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
- * and associated documentation files (the "Software"), to deal in the Software without restriction, 
- * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
- * is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies 
- * or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
- * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+// The watch id references the current `watchAcceleration`
+    var watchID = null;
 
-function roundNumber(num) {  // Helper function
-    var dec = 3;
-    var result = Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
-    return result;
-}
+    // Wait for device API libraries to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-// api-accelerometer
-var accelerationWatch = null;
-function updateAcceleration(acceleration) {
-    document.getElementById('x').innerHTML = roundNumber(acceleration.x);
-    document.getElementById('y').innerHTML = roundNumber(acceleration.y);
-    document.getElementById('z').innerHTML = roundNumber(acceleration.z);
-    document.getElementById('accel_timestamp').innerHTML = acceleration.timestamp;
-}
-function toggleAccel() {
-    if (accelerationWatch !== null) {
-        navigator.accelerometer.clearWatch(accelerationWatch);
-        updateAcceleration({
-            x : "",
-            y : "",
-            z : "",
-            timestamp: ""
-        });
-        accelerationWatch = null;
-    } else {
-        var options = {};
-        options.frequency = 1000;
-        accelerationWatch = navigator.accelerometer.watchAcceleration(
-                updateAcceleration, function(ex) {
-                    alert("Accelerometer Error!");
-                }, options);
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        startWatch();
     }
-}
 
-function getAccel() {
-    if (accelerationWatch !== null) {
-        navigator.accelerometer.clearWatch(accelerationWatch);
-        updateAcceleration({
-            x : "",
-            y : "",
-            z : "",
-            timestamp: ""
-        });
-        accelerationWatch = null;
+    // Start watching the acceleration
+    //
+    function startWatch() {
+
+        // Update acceleration every 3 seconds
+        var options = { frequency: 3000 };
+
+        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
     }
-    navigator.accelerometer.getCurrentAcceleration(
-            updateAcceleration, function(ex) {
-                alert("Accelerometer Error!");
-            });
-}
+
+    // Stop watching the acceleration
+    //
+    function stopWatch() {
+        if (watchID) {
+            navigator.accelerometer.clearWatch(watchID);
+            watchID = null;
+        }
+    }
+
+    // onSuccess: Get a snapshot of the current acceleration
+    //
+    function onSuccess(acceleration) {
+        var element = document.getElementById('coords');
+        element.innerHTML = 'Acceleration X: ' + acceleration.x         + '<br />' +
+                            'Acceleration Y: ' + acceleration.y         + '<br />' +
+                            'Acceleration Z: ' + acceleration.z         + '<br />' +
+                            'Timestamp: '      + acceleration.timestamp + '<br />';
+    }
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+     
